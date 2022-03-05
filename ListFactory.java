0ee43b2e05e4b8246dcc16c97207661ef4d04325
Furlan11 <mitofurlan13@gmail.com>
2022-03-04 21:10:13 -0600

    Update ListFactory.java

--- ListFactory.java
+++ ListFactory.java
@@ -1,4 +1,13 @@
 
 public class ListFactory {
-
+	  public static IList construir(String tipo) {
+	        switch(tipo) {
+	            case "Lista simple":
+	                return new SingleLinkedList();
+	            case "Lista doble":
+	                return new DoubleLinkedList();
+	            default:
+	                return null;
+	        }
+	    }
 }
