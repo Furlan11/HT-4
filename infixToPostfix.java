cb2a02d6b91626f1170e814fae854c822b7ab93f
Wilfredo-gallegos <70668558+Wilfredo-gallegos@users.noreply.github.com>
2022-03-04 18:31:19 -0600

    Add files via upload

--- infixToPostfix.java
+++ infixToPostfix.java
@@ -1,4 +1,78 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class infixToPostfix {
 
+	private IStack<String> Stack;
+	
+	public String leer_archivo(String x) {
+		File archivo = new File(x);
+		BufferedReader obj=null;
+		try {
+			obj = new BufferedReader(new FileReader(archivo));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("La ruta no se puede leer");;
+		}
+		String operaciones = "";
+		try {
+			operaciones= obj.readLine();
+		} catch (Exception e) {
+		      System.out.println("No tiene datos el archivo");
+	    } 
+		return operaciones;
+		
+	}
+
+	public static boolean isNumeric(String ope) {
+	    try {
+	        double d = Double.parseDouble(ope);
+	    } catch (NumberFormatException nfe) {
+	        return false; //Error no es numerico
+	    }
+	    return true; //Es numerico
+	}
+	
+	public String input(String opcion,String T) {
+		String operacion = T;
+		if(opcion=="lista") {
+			StackUsingLinkedList<String> lista = new StackUsingLinkedList<String>();
+			String valor = "";
+			
+			
+		}else {
+			if(opcion=="double") {
+				StackUsingDoubleLinkedList<String> doublelist = new StackUsingDoubleLinkedList<String>();
+			}
+		}
+		
+		
+		return T;
+	}
 }
+/*
+ * 			for(int i = 0; i< operacion.length(); i++) {
+				if(String.valueOf(operacion.charAt(i))=="(") {
+					
+				}	
+				
+				
+				
+				
+				boolean llave= false;
+				int contador2=i;
+				if(i<operacion.length()-1) {
+				llave= true;}
+				while (llave==true) {
+					contador2= contador2+1;
+					if(isNumeric(String.valueOf(operacion.charAt(contador2)))!=true) {
+						 valor=valor+String.valueOf(operacion.charAt(contador2));		 
+					}
+					else {llave=false;}
+					i=contador2;
+				}
+				
+				
+			}*/
